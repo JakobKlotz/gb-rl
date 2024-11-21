ed1524fe317398b5219115cc711411322de24f59
Jakob Klotz <jakob.klotz@mci.edu>
2024-11-21 15:28:21 +0100

    feat: callback to save the model after `n` timesteps

---
+++ envs/callback.py
@@ -0,0 +1,20 @@
+import os
+from stable_baselines3.common.callbacks import BaseCallback
+from pathlib import Path
+
+class TrainAndLoggingCallback(BaseCallback):
+    def __init__(self, check_freq, save_path, model_prefix: str, verbose=1):
+        super(TrainAndLoggingCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.save_path = save_path
+        self.model_prefix = model_prefix
+
+    def _init_callback(self):
+        if self.save_path is not None:
+            Path(self.save_path).mkdir(exist_ok=True)
+
+    def _on_step(self):
+        if self.n_calls % self.check_freq == 0:
+            model_path = Path(self.save_path) / f"{self.model_prefix}_{self.n_calls}"
+            self.model.save(str(model_path))
+        return True
