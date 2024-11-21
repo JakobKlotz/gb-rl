e345167e78569d90c52dae06d31661181ebfee23
Jakob Klotz <jakob.klotz@mci.edu>
2024-11-21 15:45:25 +0100

    refactor: added type hints to `TrainAndLoggingCallback`

--- envs/callback.py
+++ envs/callback.py
@@ -4,17 +4,24 @@ from stable_baselines3.common.callbacks import BaseCallback
 
 
 class TrainAndLoggingCallback(BaseCallback):
-    def __init__(self, check_freq, save_path, model_prefix: str, verbose=1):
+    def __init__(
+        self,
+        check_freq: int,
+        *,
+        save_path: str,
+        model_prefix: str,
+        verbose: int = 1,
+    ) -> None:
         super(TrainAndLoggingCallback, self).__init__(verbose)
         self.check_freq = check_freq
         self.save_path = save_path
         self.model_prefix = model_prefix
 
-    def _init_callback(self):
+    def _init_callback(self) -> None:
         if self.save_path is not None:
             Path(self.save_path).mkdir(exist_ok=True)
 
-    def _on_step(self):
+    def _on_step(self) -> bool:
         if self.n_calls % self.check_freq == 0:
             model_path = (
                 Path(self.save_path) / f"{self.model_prefix}_{self.n_calls}"
